5a0843b8d9b06e1add5aa540591752f4ea1493be
Shubh Sharma <69891912+shubhsharma19@users.noreply.github.com>
2023-12-06 20:19:44 +0530

    Update timeLeftOnEarthCounter.js

--- timeLeftOnEarthCounter.js
+++ timeLeftOnEarthCounter.js
@@ -11,7 +11,7 @@ if(isNaN(yourAge) || yourAge <= 0 || yourAge > 75 ) {
     leftTime = lifeInWeeks(averageAgeofHuman, yourAge);
 }
 
-alert(`You have ${leftTime[0]} in years, ${leftTime[1]} in months, ${leftTime[2]} in weeks and ${leftTime[3]} in days left.`);
+alert(`You have ${leftTime[0]} in years, ${leftTime[1]} in months, ${leftTime[2]} in weeks and ${leftTime[3]} in days left on earth.`);
 
 function lifeInWeeks(averageAgeofHuman, yourAge) {
     
