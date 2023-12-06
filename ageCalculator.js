aa16be06d2ebbd5fc8649f71e0da0792086517df
shubhsharma19 <mail.shubhsharma19@gmail.com>
2023-12-06 15:08:27 +0530

    add: edgecases

--- ageCalculator.js
+++ ageCalculator.js
@@ -1,6 +1,15 @@
-const yourAge = prompt("Enter your age");
 const averageAgeofHuman = 75;
-let leftTime = lifeInWeeks(averageAgeofHuman, yourAge);
+let leftTime;
+
+const yourAge = prompt("Enter your age");
+
+// edgecase -> age is number or age is more than 0 or age is more than 75
+
+if(isNaN(yourAge) || yourAge <= 0 || yourAge > 75 ) {
+    alert("please enter a valid age (between 1 to 75)");
+} else {
+    leftTime = lifeInWeeks(averageAgeofHuman, yourAge);
+}
 
 alert(`You have ${leftTime[0]} in years, ${leftTime[1]} in months, ${leftTime[2]} in weeks and ${leftTime[3]} in days left.`);
 
@@ -11,7 +20,7 @@ function lifeInWeeks(averageAgeofHuman, yourAge) {
     let weeksLeft = yearsLeft*52;
     let daysLeft = yearsLeft*365;
 
-    // returns an array of years, months, weeks and days
+    // returns an array of years, months, weeks and days left
     return new Array(yearsLeft, monthsLeft, weeksLeft, daysLeft);
 }
 
