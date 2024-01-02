b873482bc783250c541ff8d36fb26380f41d8a5a
shubhsharma19 <mail.shubhsharma19@gmail.com>
2024-01-02 13:54:38 +0530

    add: fileSpaceCleaner.js

---
+++ 2_medium/fileSpaceCleaner.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'file.txt');
+
+fs.readFile(filePath, 'utf-8', (_err, data) => {
+    let dataFromFile;
+    try {
+        dataFromFile = data;
+        dataFromFile = dataFromFile.trim()
+        let cleanData = dataFromFile.replace(/\s+/g, ' ');
+
+        fs.writeFile(filePath, cleanData, (_err) => {
+            try {
+                console.log('Successfully Written to the file!')
+            } catch (err) {
+                console.log(err);
+            }
+        })
+    } catch(err) {
+        console.log(err);
+    }
+})
