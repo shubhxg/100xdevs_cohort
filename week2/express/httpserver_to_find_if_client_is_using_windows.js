19702096b05b85c0cad58637c04742d4720bc928
Shubh Sharma <69891912+shubhsharma19@users.noreply.github.com>
2024-01-10 01:41:28 +0530

    Create httpserver_to_find_if_client_is_using_windows.js

---
+++ week2/express/httpserver_to_find_if_client_is_using_windows.js
@@ -0,0 +1,24 @@
+// requirements: 
+// npm init -y, 
+// npm install express
+
+const express = require('express');
+const { stringify } = require('qs');
+const app = express();
+let port = 3000;
+
+// starts the server and makes it listen on the port 
+app.listen(port, ()=>{
+    console.log(`server started at ${port}`);
+});
+
+// response sent to the user at route '/' which is root
+app.get('/', function(req, res) {
+
+  // returns an object, info such as browser and os of the client
+  const data = req.rawHeaders;
+
+  // this line checks if the data sent by the browser has word Windows in it or not.
+  // first turned the object into a string then splitted the words into an array and then checked if array has Windows.
+  const isWindowsUser = JSON.stringify(data).split(" ").includes("Windows");
+})
