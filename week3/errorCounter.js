2a9f535d21c3afe0c2f9b194f5d49481350e30a0
Shubh Sharma <69891912+shubhsharma19@users.noreply.github.com>
2024-01-30 00:42:06 +0530

    Create errorCounter.js

---
+++ week3/errorCounter.js
@@ -0,0 +1,18 @@
+app.get('/user', function(req, res) {
+  throw new Error("User not found");
+  res.status(200).json({ name: 'john' });
+});
+
+app.post('/user', function(req, res) {
+  res.status(200).json({ msg: 'created dummy user' });
+});
+
+app.get('/errorCount', function(req, res) {
+  res.status(200).json({ errorCount });
+});
+
+app.use((err, req, res, next) => {
+  errorCount++;
+  res.sendStatus(404);
+  next();
+});
